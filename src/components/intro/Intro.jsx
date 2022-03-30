636ec8e7880cd6a35fa5b8d257ead7de8ed4a97e
Sashank Shukla <ayaz.shukla@gmail.com>
2022-03-29 17:14:01 -0700

    add photo

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,15 @@
 import "./Intro.css"
+import my_picture from "assets/personalphoto.png";
 
 export default function Intro() {
   return (
     <div className="intro" id="intro">
-      Intro
+      <div className="left">
+        <div className="imgContainer">
+          <img src={my_picture} alt="my photo"></img>
+        </div>
+      </div>
+      <div className="right"></div>
     </div>
   )
 }
