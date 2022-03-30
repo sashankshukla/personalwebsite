cef25b80ed930d2f6587aa56edf759298241f3e7
Sashank Shukla <ayaz.shukla@gmail.com>
2022-03-30 01:59:08 -0700

    phase skills

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -3,7 +3,32 @@ import "./Skills.css"
 export default function Skills() {
   return (
     <div className="skills" id="skills">
-        Skills
+        <div className="top">
+         <h1>Skills</h1>
+        </div>
+        <div className="wrapper">
+         <div className="left">
+           <div className="skill">
+             <img src="assets/reacticon.png" alt=""></img>
+             React
+           </div>
+           <div className="skill">C++</div>
+           <div className="skill">JavaScript</div>
+           <div className="skill">Data Structures</div>
+         </div>
+         <div className="middle">
+            <div className="skill">HTML</div>
+            <div className="skill">Java</div>
+            <div className="skill">CSS</div>
+            <div className="skill">Algorithms</div>
+         </div>
+         <div className="right">
+           <div className="skill">CSS</div>
+           <div className="skill">Python</div>
+           <div className="skill">C</div>
+           <div className="skill">Git</div>
+         </div>
+        </div>
     </div>
   )
 }
