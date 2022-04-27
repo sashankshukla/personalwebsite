fcb390f74ba1ce3722f5f172c40e52997f083d9e
Sashank Shukla <ayaz.shukla@gmail.com>
2022-04-26 22:57:17 -0700

    restarting work commit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,12 +2,6 @@ import "./Portfolio.css"
 import { useState } from "react"; 
 
 export default function Portfolio() {
-  const items = [{
-    title: "May 1940",
-    contentTitle: "Dunkirk",
-    contentText:"Men of the British Expeditionary Force (BEF) wade out to a destroyer during the evacuation from Dunkirk.",
-    contentDetailedText: "On 10 May 1940, Hitler began his long-awaited offensive in the west...",
-  }];
   return (
     <div className="portfolio" id="portfolio">
       <h1>Me</h1>
