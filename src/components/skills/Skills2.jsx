4c30b7a6535a88cce49197414fc438d41a09f169
Sashank Shukla <ayaz.shukla@gmail.com>
2022-05-22 21:45:26 -0700

    finish new skills component

--- src/components/skills/Skills2.jsx
+++ src/components/skills/Skills2.jsx
@@ -5,7 +5,14 @@ import "./Skills2.css"
 function Skills2() {
   return (
     <div className='skills2'>
-    <h1>Skills</h1>
+      <div className="left">
+        <h1>S</h1>
+        <h1>k</h1>
+        <h1>i</h1>
+        <h1>l</h1>
+        <h1>l</h1>
+        <h1>s</h1>
+      </div>
     <div className="img_div">
     <img src={skill_image} alt="my skills"></img>
     </div>
